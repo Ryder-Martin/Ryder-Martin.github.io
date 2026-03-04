1601a28b4305ce0ee63812f08aaee773953e8447
Ryder-Martin <2202472@spsd.sk.ca>
2026-03-04 12:18:13 -0600

    perlin noise work
    changed random to noise and got the left and right arrows to increase and decrease the size of the rectangles

--- 07 random vs noise/sketch.js
+++ 07 random vs noise/sketch.js
@@ -1,15 +1,64 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// random vs noise
+// ryder martin
+// 2/27/2026
+//looking at unpredictability, speciffically the differnce between uniformely distrubuted numbers and perlin noise
 
+//global variables
+// let seed;
+let d1, d2;
+let minSize = 5; let maxSize = 200;
+let x1, x2, y, y2
+
+// variables for using noise
+let noiseTime = 5;
+let noiseSpeed = 0.005;
+let yNoiseTime = 10, yNoiseSpeed = 0.1;
+//noise speed controls how connected the randm noise values are
+
+let ySpeed = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x1 = width * 0.3; y1 = height * 0.5; x2 = width * 0.7; y2 = height * 0.5;
+  // frameRate(3);
+  // seed = random(1,100);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  // randomSeed(seed);
+  // stars();
+  // randomCircle();
+  ySpeed = noise(yNoiseTime);
+  ySpeed = map(ySpeed,0,1,-5,5);
+  y2 = ySpeed;
+  yNoiseTime += yNoiseSpeed;
+  noiseCircle();
+}
+
+function stars() {
+  //use random to generate 100 stars
+  fill(255);
+  for (let i = 0; i < 100; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    circle(x, y, 3);
+  }
+
+}
+
+function randomCircle() {
+  //draw a fixed circle with randomly changing diameter
+  fill("orange");
+  d1 = random(minSize, maxSize);
+  circle(x1,y1,d1);
+}
+
+function noiseCircle(){
+  //draw a fixed circle with randomly changing but smooth diameter
+  fill(200,150,50);
+  d2 = noise(noiseTime);//yields value between 0 and 1
+  d2 = map(d2,0,1,minSize, maxSize);
+  circle(x2,y2,d2);
+  noiseTime += noiseSpeed;
 }
---
+++ 09 perlin noise generator terrain/sketch.js
@@ -0,0 +1,46 @@
+// perlin noise terrain generator
+// Ryer MArtin
+// 3/3/2026
+let rectWidth = 1;
+let noiseTime = 5; 
+let noiseSpeed = 0.01;
+
+function setup() {
+  createCanvas(250, 370);
+  fill(0);
+}
+
+function generateTerrain() {
+  //using a loop, construct a number of side byside rectangles with random heights to be 2D terrain
+  for (let x = 0; x < width; x += rectWidth) {
+    // generate random (negative height)
+    // replace with noise
+    let rectHeight = noise(noiseTime);      //random(0, height * 0.75);
+    rectHeight = map(rectHeight,0,1,0,300);
+    noiseTime += noiseSpeed;
+    rect(x, height, rectWidth, -rectHeight);
+  }
+}
+
+function widthChanger(){
+  if(keyIsPressed){
+    if(keyCode===LEFT_ARROW){
+      rectWidth -=0.1;
+      if(rectWidth < 1){
+        rectWidth = 1;
+      }
+    }
+    if(keyCode===RIGHT_ARROW){
+      rectWidth += 0.1;
+    }
+  }
+  
+}
+
+
+function draw() {
+  noiseTime = 5;
+  background(220);
+  generateTerrain();
+  widthChanger();
+}
